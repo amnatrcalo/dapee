002716d121e333c92bc49d53ecfcf1dda4423fc2
Evelin <epiljug1@etf.unsa.ba>
2023-03-22 23:12:36 +0100

    adding report to administration

--- administration/src/main/java/com/forum/microservice/administration/entity/ReportEntity.java
+++ administration/src/main/java/com/forum/microservice/administration/entity/ReportEntity.java
@@ -1,2 +1,46 @@
-package com.forum.microservice.administration.entity;public class ReportEntity {
+package com.forum.microservice.administration.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "report")
+public class ReportEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "user")
+    private UserEntity user;
+
+    @ManyToOne(
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "post")
+    private PostEntity post;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public PostEntity getPost() {
+        return post;
+    }
+
+    public void setPost(PostEntity post) {
+        this.post = post;
+    }
 }
