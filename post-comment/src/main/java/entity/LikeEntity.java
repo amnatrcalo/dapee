f39b923ad2cfa00e484a51a34f287f85937e3d1b
esarak1 <esarak1@etf.unsa.ba>
2023-03-29 11:33:58 +0200

    Create entities for post-comment

---
+++ post-comment/src/main/java/entity/LikeEntity.java
@@ -0,0 +1,70 @@
+package entity;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name = "like")
+public class LikeEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "voter_id")
+    private UserEntity voter;
+
+    @ManyToOne(
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "post_id")
+    private PostEntity post;
+
+    @ManyToOne(
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "comment_id")
+    private PostEntity comment;
+    //napravi onetomany post i comment
+    @Column(name="liked")
+    private boolean liked;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public UserEntity getVoter() {
+        return voter;
+    }
+
+    public void setVoter(UserEntity voter) {
+        this.voter = voter;
+    }
+
+    public PostEntity getPost() {
+        return post;
+    }
+
+    public void setPost(PostEntity post) {
+        this.post = post;
+    }
+
+    public PostEntity getComment() {
+        return comment;
+    }
+
+    public void setComment(PostEntity comment) {
+        this.comment = comment;
+    }
+
+    public boolean isLiked() {
+        return liked;
+    }
+
+    public void setLiked(boolean liked) {
+        this.liked = liked;
+    }
+}
---
+++ post-comment/src/main/java/entity/PostEntity.java
@@ -0,0 +1,105 @@
+package entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "post")
+public class PostEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @ManyToOne(
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "creator_id")
+    private UserEntity creator;
+
+    @ManyToOne(
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "subforum_id")
+    private SubforumEntity subforum;
+
+    @OneToMany(
+            mappedBy = "post",
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<CommentEntity> comments;
+
+    @OneToMany(
+            mappedBy = "post",
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<ReportEntity> reports;
+    public PostEntity() {}
+
+    public PostEntity(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public UserEntity getCreator() {
+        return creator;
+    }
+
+    public void setCreator(UserEntity creator) {
+        this.creator = creator;
+    }
+
+    public SubforumEntity getSubforum() {
+        return subforum;
+    }
+
+    public void setSubforum(SubforumEntity subforum) {
+        this.subforum = subforum;
+    }
+
+    public List<CommentEntity> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentEntity> comments) {
+        this.comments = comments;
+    }
+
+    public List<ReportEntity> getReports() {
+        return reports;
+    }
+
+    public void setReports(List<ReportEntity> reports) {
+        this.reports = reports;
+    }
+    @Override
+    public String toString() {
+        return "PostEntity{" + "title='" + title + '\'' + ", content='" + content + '\'' + '}';
+    }
+}
