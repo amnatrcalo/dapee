f268d4077660e9bf9645569145ffdaeb09dc6d7b
esarak1 <esarak1@etf.unsa.ba>
2023-03-30 05:21:38 +0200

    Fixed a problem with connection to database

--- post-comment/src/main/java/com/forum/microservice/postcomment/controller/PostController.java
+++ post-comment/src/main/java/com/forum/microservice/postcomment/controller/PostController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/administration")
+@RequestMapping("/post-com")
 public class PostController {
   private PostService postService;
 
--- post-comment/src/main/java/com/forum/microservice/postcomment/controller/SubforumController.java
+++ post-comment/src/main/java/com/forum/microservice/postcomment/controller/SubforumController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/administration")
+@RequestMapping("/post-com")
 public class SubforumController {
   private SubforumService subforumService;
 
